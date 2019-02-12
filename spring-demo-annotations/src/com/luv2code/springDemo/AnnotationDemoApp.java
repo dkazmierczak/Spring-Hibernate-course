80c3f8693b8344a6a7db26c02f365bce090f30e5
dkazmierczak <dawkaz66@gmail.com>
2019-02-12 13:04:57 +0100

    some changes

---
+++ spring-demo-annotations/src/com/luv2code/springDemo/AnnotationDemoApp.java
@@ -0,0 +1,22 @@
+package com.luv2code.springDemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+    public static void main(String[] args) {
+
+        //read spring config file
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        //get the bean from spring container
+        Coach theCoach = context.getBean("thatSillyCoach", Coach.class);
+
+        //call a method in the bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        //close the context
+        context.close();
+    }
+}
