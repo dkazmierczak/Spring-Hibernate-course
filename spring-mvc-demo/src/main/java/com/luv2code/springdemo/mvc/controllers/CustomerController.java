147dd0d44bedb134e2aaf73f31370325276919f6
dkazmierczak <dawkaz66@gmail.com>
2019-03-27 20:54:36 +0100

    validation in controller class

---
+++ spring-mvc-demo/src/main/java/com/luv2code/springdemo/mvc/controllers/CustomerController.java
@@ -0,0 +1,36 @@
+package com.luv2code.springdemo.mvc.controllers;
+
+import com.luv2code.springdemo.mvc.Customer;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.naming.Binding;
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @RequestMapping("/showForm")
+    public String showForm(Model model){
+
+        model.addAttribute("customer", new Customer());
+
+        return "customer-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm(
+            @Valid @ModelAttribute("customer") Customer theCustomer,
+            BindingResult bindingResult
+    ){
+
+        if(bindingResult.hasErrors())
+            return "customer-form";
+        else
+            return "customer-confirmation";
+    }
+}
---
+++ spring-mvc-demo/target/spring-mvc-demo/WEB-INF/view/customer-confirmation.jsp
@@ -0,0 +1,17 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<html>
+
+<head>
+
+    <title>Title</title>
+
+</head>
+
+<body>
+
+The customer is confirmed: ${customer.firstName} ${customer.lastName}
+
+</body>
+
+</html>
