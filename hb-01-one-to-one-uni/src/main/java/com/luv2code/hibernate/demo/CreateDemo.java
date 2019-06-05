11fe073fdded3e4da3f2edfc4d9f94e744472e0d
Dawid Kaźmierczak <dawid@macbook-pro-dawid.home>
2019-06-05 07:41:49 +0200

    build main app for one to one

--- hb-01-one-to-one-uni/src/main/java/com/luv2code/hibernate/demo/CreateDemo.java
+++ hb-01-one-to-one-uni/src/main/java/com/luv2code/hibernate/demo/CreateDemo.java
@@ -1,33 +1,46 @@
 package com.luv2code.hibernate.demo;
 
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
 import com.luv2code.hibernate.demo.entity.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class CreateStudentDemo {
+public class CreateDemo {
 
     public static void main(String[] args) {
 
         //create session factory
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
-                .addAnnotatedClass(Student.class)
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
                 .buildSessionFactory();
 
         //create session
         Session session = factory.getCurrentSession();
         try{
-            //create a student object
-            System.out.println("Creating new student object...");
-            Student tempStudent = new Student("Paul", "Wall", "paul@luv2code.com");
+
+            //create objects
+            Instructor instructor = new Instructor(
+                    "Chad",
+                    "Darby",
+                    "darby@luve2code.com"
+            );
+
+            InstructorDetail instructorDetail = new InstructorDetail(
+                    "http://www.luve2code.come/youtube",
+                    "Luv 2 code!!"
+            );
+
+            //associate the objects
+            instructor.setInstructorDetail(instructorDetail);
 
             //start a transaction
             session.beginTransaction();
 
-            //save the student object
-            System.out.println("Saving the student...");
-            session.save(tempStudent);
+
 
             //commit transaction
             session.getTransaction().commit();
