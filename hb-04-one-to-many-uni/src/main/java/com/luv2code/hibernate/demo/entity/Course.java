0f8f956294fe425b2bf52345996b791fee914736
Dawid Kaźmierczak <dawid@macbook-pro-dawid.home>
2019-06-09 12:57:24 +0200

    updateg course class

--- hb-04-one-to-many-uni/src/main/java/com/luv2code/hibernate/demo/entity/Course.java
+++ hb-04-one-to-many-uni/src/main/java/com/luv2code/hibernate/demo/entity/Course.java
@@ -2,6 +2,8 @@ package com.luv2code.hibernate.demo.entity;
 
 import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "course")
@@ -24,6 +26,10 @@ public class Course {
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
     @JoinColumn(name = "instructor_id")
     private Instructor instructor;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "course_id")
+    private List<Review> reviews;
 
     public Course(){}
 
@@ -53,6 +59,22 @@ public class Course {
 
     public void setInstructor(Instructor instructor) {
         this.instructor = instructor;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    //add a convinience method
+    public void addReview(Review theReview){
+        if (reviews == null){
+            reviews = new ArrayList<>();
+        }
+        reviews.add(theReview);
     }
 
     @Override
